bdef37140ba405e9bd7d1177e3554ed3b5fdcd2e
iichan2 <hanikanda@yahoo.co.jp>
2019-10-01 12:46:06 +0900

    au date

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,12 +5,11 @@ class Group < ApplicationRecord
   
   validates :name, presence: true, uniqueness: true
 
-def index
-end
-
-def creat
-end
-
-
-
+  def show_last_message
+    if (last_message = messages.last).present?
+      last_message.content? ? last_message.content : '画像が投稿されています'
+    else
+      'まだメッセージはありません。'
+    end
+  end
 end
