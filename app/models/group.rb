af047766156aa324d102da434221113d95ebfa26
iichan2 <hanikanda@yahoo.co.jp>
2019-10-01 11:02:24 +0900

    create group and message function

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,9 @@
 class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
-  # validates :name, presence: true, uniqueness: true
+  has_many :messages
+  
+  validates :name, presence: true, uniqueness: true
 
 def index
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'groups#index'
+  root  'groups#index'
   resources :users, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
+ 
     resources :messages, only: [:index, :create]
   end
 end
