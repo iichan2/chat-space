dc5839fb4d3543a22f3f252dccb19635b82a07f5
iichan2 <55778066+iichan2@users.noreply.github.com>
2019-10-02 15:48:28 +0900

    Revert "グループ機能の作成"

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,6 @@ module ChatSpace
       g.javascripts false
       g.helper false
       g.test_framework false
-      end
-    config.i18n.default_locale = :ja
-    config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
+    end
   end
 end
