173d3da85ef731693255d02e88e132721caa6172
iichan2 <hanikanda@yahoo.co.jp>
2019-10-08 15:33:11 +0900

    commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,15 @@ class UsersController < ApplicationController
     end
   end
 
+def index
+  @users = User.where('name LIKE(?)', "#{params[:keyword]}%")
+  respond_to do |format|
+    format.html
+    format.json
+end  
+
+
+
   private
 
   def user_params
