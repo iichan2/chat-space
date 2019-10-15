e43a02a08ddcd49e9557b30c078c1c39db54db8a
iichan2 <hanikanda@yahoo.co.jp>
2019-10-15 13:01:53 +0900

    commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :rbenv_ruby, '2.5.1' #このアプリで使用しているrubyのバージ�
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/chatspace.pem']  ※例：~/.ssh/key_pem.pem
+                  keys: ['~/.ssh/chatspace.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
