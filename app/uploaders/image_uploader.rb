3f1e41241ef13862c46484209b4ca61ba79345e1
iichan2 <hanikanda@yahoo.co.jp>
2019-10-16 12:20:46 +0900

    commit

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,6 +5,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   
   process resize_to_fit: [800, 800]
   # Choose what kind of storage to use for this uploader:
+  storage :fog
   storage :file
   # storage :fog
 
